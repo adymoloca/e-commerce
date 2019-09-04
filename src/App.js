cf263c396ccbdc350292f65b8c06c88ec2ebefce
adymoloca <ady.moloca@yahoo.com>
2019-09-05 00:29:57 +0300

    Create and styles the sign in form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import { Switch, Route } from 'react-router-dom';
+
+import Header from './components/header/header.component';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import './App.css';
 
 function App() {
@@ -10,8 +15,9 @@ function App() {
     <div>
       <Header />
       <Switch>
-        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/' component={HomePage} /> 
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    this.state = {
+        name: '',
+        email: '',
+        password: ''
+    }
+}
